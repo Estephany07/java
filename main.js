c41e2eee1a492b6ddbe13fbaa811561a8f6fa2cd
Estephany07 <106981546+Estephany07@users.noreply.github.com>
2023-11-08 11:25:07 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,44 +1,18 @@
-function tocaSom(idElementoAudio) {
+function tocaSom (idElementoAudio) {
     document.querySelector(idElementoAudio).play();
-};
+}
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-for (contador = 0; contador < listaDeTeclas.length; contador++) {
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const IdAudio = `#som_${instrumento}`;
+    const idAudio = `#som_${instrumento}`; //template string
 
     tecla.onclick = function () {
-        tocaSom(IdAudio);
+        tocaSom(idAudio);
     }
-
-
-    tecla.onkeydown = function () {
-        tecla.classList.add("ativa");
-        ///////////////////
-        tocaSom(IdAudio);
-        setTimeout(() => {
-            tecla.classList.remove("ativa");
-        }, 200);
-        ///////////////////
-    }
-
-
-
-
-
-
-
-
-    /*tecla.onkeydown = function () {
-        tecla.classList.add("ativa");
-
-    }
-
-    tecla.onkeyup = function () {
-        tecla.classList.remove("ativa");
-    }*/
 
 }
